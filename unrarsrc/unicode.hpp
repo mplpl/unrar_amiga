464b97d23d0a3200237211a2ae7642bca3e919a0
mplpl <marcin.labenski@gmail.com>
2019-03-24 10:08:00 +0000

    utf8 normalization for names in rar file

--- unrarsrc/unicode.hpp
+++ unrarsrc/unicode.hpp
@@ -64,6 +64,7 @@ inline void copychrd(char *dest,const char *src) {*dest=*src;}
 #endif
 
 #ifdef _AMIGA
+#include <utf8proc.h>
 bool WideToLocal(const wchar *Src,char *Dest,size_t DestSize);
 bool LocalToWide(const char *Src,wchar *Dest,size_t DestSize);
 #endif
