0e908f455d68551c81d94816f5bd98cc69772ade
mlabenski <marcin.labenski@pl.ibm.com>
2019-12-20 09:18:23 +0100

    Dealing with const char in iconv

--- unrarsrc/unicode.cpp
+++ unrarsrc/unicode.cpp
@@ -709,7 +709,11 @@ bool WideToLocal(const wchar *Src,char *Dest,size_t DestSize)
       return true;
     }
   }
+#if defined(__AROS__)
   char *inPtr = (char *)lineBufNorm;
+#else
+  const char *inPtr = (const char *)lineBufNorm;
+#endif
   char *outPtr = Dest;
   size_t inSize = strlen((char *)lineBufNorm);
   size_t outSize = DestSize;
@@ -762,7 +766,11 @@ bool LocalToWide(const char *Src,wchar *Dest,size_t DestSize)
     }
   }
   char buf[4 * DestSize + 1];
+#if defined(__AROS__)
   char *inPtr = (char *)Src;
+#else
+  const char *inPtr = Src;
+#endif
   char *outPtr = (char *)buf;
   size_t inSize = strlen((char *)Src);
   size_t outSize = 4 * DestSize;
